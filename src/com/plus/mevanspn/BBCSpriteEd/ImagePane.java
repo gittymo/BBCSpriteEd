53e49ab8f2a5f8b2748c62be39b2cf3e8d2b0e96
gittymo <mooglestuff@gmail.com>
2021-05-04 10:29:13 +0100

    Small change to package name, so that it is compatible with the new top level directory name.

---
+++ src/com/plus/mevanspn/BBCSpriteEd/ImagePane.java
@@ -0,0 +1,127 @@
+package com.plus.mevanspn.BBCSpriteEd;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+final public class ImagePane extends JPanel implements MouseListener, MouseMotionListener {
+    public ImagePane(MainFrame parent) {
+        super();
+        this.parent = parent;
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+        BBCSpriteFrame activeImage = null;
+    }
+
+    private Rectangle GetImageArea() {
+        final BBCSpriteFrame activeImage = parent.GetActiveFrame();
+        final float zoom = parent.GetZoom();
+        if (activeImage == null) return null;
+        final int panelWidth = this.getWidth();
+        final int panelHeight = this.getHeight();
+        final int imageWidth = (int) (activeImage.GetWidth() * zoom * activeImage.GetHorizontalPixelRatio());
+        final int imageHeight = (int) (activeImage.GetHeight() * zoom);
+        return new Rectangle((panelWidth - imageWidth) / 2, (panelHeight - imageHeight) / 2, imageWidth, imageHeight);
+    }
+
+    private boolean ClickInImageArea(int x, int y) {
+        Rectangle r = GetImageArea();
+        return (r != null && x >= r.x && x <= r.x + r.width && y >= r.y && y <= r.y + r.height);
+    }
+
+    private Point GetImageXY(int x, int y) {
+        if (!ClickInImageArea(x, y)) return null;
+        final Rectangle r = GetImageArea();
+        if (r != null) {
+            final float zoom = parent.GetZoom();
+            final BBCSpriteFrame image = parent.GetActiveFrame();
+            Point p = new Point();
+            p.x = (int) ((x - r.x) / (zoom * image.GetHorizontalPixelRatio()));
+            p.y = (int) ((y - r.y) / zoom);
+            return p;
+        }
+        return null;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        final float zoom = parent.GetZoom();
+        final BBCSpriteFrame activeImage = parent.GetActiveFrame();
+        if (activeImage != null) {
+            Rectangle r = GetImageArea();
+            if (r != null) {
+                g2.drawImage(activeImage.GetRenderedImage(zoom), r.x, r.y, r.width, r.height, null);
+                if (zoom > 4) {
+                    g2.setColor(new Color(0, 0, 128, 255));
+                    g2.setStroke(new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER,
+                            1.0f, new float[]{1.0f}, 1.0f));
+                    for (float x = zoom * activeImage.GetHorizontalPixelRatio(); x < r.width; x += zoom * activeImage.GetHorizontalPixelRatio()) {
+                        g2.drawLine((int) x + r.x, r.y, (int) x + r.x, r.y + r.height);
+                    }
+                    for (float y = zoom; y < r.height; y += zoom) {
+                        g2.drawLine(r.x, (int) y + r.y, r.x + r.width, (int) y + r.y);
+                    }
+                    g2.setStroke(new BasicStroke());
+                }
+                g2.setColor(Color.BLACK);
+                g2.drawRect(r.x - 1, r.y - 1, r.width + 1, r.height + 1);
+            }
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        final BBCSpriteFrame activeImage = parent.GetActiveFrame();
+        if (activeImage != null) {
+            final Point p = GetImageXY(e.getX(), e.getY());
+            if (p != null) {
+                activeImage.SetPixel(p.x, p.y, parent.GetActiveColourIndex());
+                repaint();
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (!mouseDown) mouseDown = true;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        mouseDown = false;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        if (mouseDown) {
+            final BBCSpriteFrame activeImage = parent.GetActiveFrame();
+            if (activeImage != null) {
+                final Point p = GetImageXY(e.getX(), e.getY());
+                if (p != null) {
+                    activeImage.SetPixel(p.x, p.y, parent.GetActiveColourIndex());
+                    repaint();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+
+    private final MainFrame parent;
+    private boolean mouseDown = false;
+}
---
+++ src/com/plus/mevanspn/BBCSpriteEd/MainFrame.java
@@ -0,0 +1,99 @@
+package com.plus.mevanspn.BBCSpriteEd;
+
+import javax.swing.*;
+import java.awt.*;
+
+final public class MainFrame extends JFrame {
+    public MainFrame() {
+        super("BBC Sprite Editor - By Morgan Evans");
+        this.sprite = null;
+        this.zoom = 16;
+        initComponents();
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LoadSprite(new BBCSprite(24, 24, BBCSprite.DisplayMode.ModeOne, this));
+    }
+
+    public static void main(String args[]) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+
+    private void initComponents() {
+        this.borderLayout = new BorderLayout();
+        this.setLayout(this.borderLayout);
+
+        this.scrollPane = new JScrollPane();
+        this.imagePane = new ImagePane(this);
+        this.scrollPane.setViewportView(imagePane);
+        this.scrollPane.setPreferredSize(new Dimension(640, 480));
+        getContentPane().add(this.scrollPane,BorderLayout.CENTER);
+
+        this.colourPickerToolbar = new ColourPickerToolbar(this);
+        getContentPane().add(this.colourPickerToolbar, BorderLayout.WEST);
+
+        this.menubar = new JMenuBar();
+        this.menubar.add(new FileMenu(this));
+        this.menubar.add(new ZoomMenu(this));
+        setJMenuBar(this.menubar);
+    }
+
+    public void LoadSprite(BBCSprite newSprite) {
+        sprite = newSprite;
+        if (sprite != null && sprite.GetActiveFrame() != null) {
+            ResizeImagePane();
+            colourPickerToolbar.CreatePaletteUsingSprite(sprite);
+        }
+    }
+
+    public float GetZoom() {
+        return zoom;
+    }
+
+    public void SetZoom(float newZoom) {
+        if (newZoom >= 0.25 && newZoom <= 32 && newZoom != zoom) zoom = newZoom;
+        ResizeImagePane();
+    }
+
+    public byte GetActiveColourIndex() {
+        return colourPickerToolbar.GetActiveColourIndex();
+    }
+
+    public BBCSpriteFrame GetActiveFrame() {
+        return sprite != null ? sprite.GetActiveFrame() : null;
+    }
+
+    public void RefreshImagePane() {
+        if (imagePane != null) imagePane.repaint();
+    }
+
+    public void ResizeImagePane() {
+        imagePane.setPreferredSize(new Dimension(
+                (int) (sprite.GetWidth() * zoom * sprite.GetHorizontalPixelRatio()) + 8,
+                (int) (sprite.GetHeight() * zoom) + 8));
+        scrollPane.revalidate();
+        imagePane.repaint();
+    }
+
+    public BBCSprite GetSprite() {
+        return sprite;
+    }
+
+    public void Quit() {
+        System.exit(-1);
+    }
+
+    private BBCSprite sprite;
+    private ImagePane imagePane;
+    private JScrollPane scrollPane;
+    private ColourPickerToolbar colourPickerToolbar;
+    private BorderLayout borderLayout;
+    private JMenuBar menubar;
+    private float zoom;
+
+    public static Color[] maskColours = new Color[] { Color.LIGHT_GRAY, Color.DARK_GRAY };
+}
