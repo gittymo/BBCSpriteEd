f9c0be4cf1008a73f7b66ea6498990af3a04d8d0
gittymo <mooglestuff@gmail.com>
2021-05-15 13:43:27 +0100

    Added ability to flip image horizontally using edit menu item.

--- src/com/plus/mevanspn/BBCSpriteEd/ui/menus/EditMenu/EditMenu.java
+++ src/com/plus/mevanspn/BBCSpriteEd/ui/menus/EditMenu/EditMenu.java
@@ -1,5 +1,8 @@
 package com.plus.mevanspn.BBCSpriteEd.ui.menus.EditMenu;
 
+import com.plus.mevanspn.BBCSpriteEd.image.BBCImage;
+import com.plus.mevanspn.BBCSpriteEd.image.BBCSprite;
+import com.plus.mevanspn.BBCSpriteEd.image.BBCSpriteFrame;
 import com.plus.mevanspn.BBCSpriteEd.ui.menus.EditMenu.RotateMenu.RotateMenu;
 import com.plus.mevanspn.BBCSpriteEd.ui.toplevel.MainFrame;
 
@@ -14,6 +17,37 @@ final public class EditMenu extends JMenu {
         add(resizeMenuItem);
         add(new JSeparator());
         add(new RotateMenu(this));
+
+        JMenuItem flipHorizontalMenuItem = new JMenuItem("Flip Horizontally");
+        flipHorizontalMenuItem.addActionListener(e -> {
+            final BBCSprite sprite = GetMainFrame().GetSprite();
+            if (sprite != null) {
+                final BBCSpriteFrame activeSpriteFrame = sprite.GetActiveFrame();
+                if (activeSpriteFrame != null && activeSpriteFrame.GetRenderedImage() != null) {
+                    final BBCImage frameImage = activeSpriteFrame.GetRenderedImage();
+                    final int imageWidth = frameImage.getWidth(), imageHeight = frameImage.getHeight();
+                    final int rasterDataSize = imageWidth * imageHeight;
+                    byte[] rasterData = new byte[rasterDataSize];
+                    frameImage.getRaster().getDataElements(0, 0, imageWidth, imageHeight, rasterData);
+                    int l = 0, r = imageWidth - 1;
+                    for (int y = 0; y < imageHeight; y++) {
+                        int ll = l, rr = r;
+                        while (ll < rr) {
+                            byte tempByte = rasterData[rr];
+                            rasterData[rr--] = rasterData[ll];
+                            rasterData[ll++] = tempByte;
+                        }
+                        l += imageWidth;
+                        r += imageWidth;
+                    }
+                    frameImage.getRaster().setDataElements(0, 0, imageWidth, imageHeight, rasterData);
+                    GetMainFrame().RefreshPanels();
+                }
+            }
+        });
+        add(flipHorizontalMenuItem);
+
+
     }
 
     public MainFrame GetMainFrame() {
